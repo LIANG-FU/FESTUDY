e63554fa26ed6285c6a0d9986438b150ecf393fe
傅良 <5381362+fu_liang@user.noreply.gitee.com>
2021-01-02 20:41:35 +0800

    提交

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ then
     git init
     git add .
     git commit -m "$1"
+    git branch -M main
+    git remote add origin git@github.com:LIANG-FU/FESTUDY.git
+    git push -u origin main
 else 
     echo "请输入git tag"
 fi
